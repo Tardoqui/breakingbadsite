40cab75032023a3c4bca236146db15992b6082b3
HenriqueTardoqui <henrique.tardoqui@gmail.com>
2022-04-28 17:00:55 -0300

    project done, css needs improvement

--- src/pages/PostDetails/index.js
+++ src/pages/PostDetails/index.js
@@ -2,15 +2,15 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./style.modules.css";
+
 function PostDetail() {
 	const params = useParams();
 	const navigate = useNavigate();
-	const [user, setUser] = useState({});
-
-
-
-
-
+	const [user, setUser] = useState({ answers: []});
+	const [form, setForm] = useState({
+		owner: "",
+		answer: "",
+	});
 
 	useEffect(() => {
 		async function fetchUser() {
@@ -19,13 +19,13 @@ function PostDetail() {
 			);
 
 			setUser(response.data);
-			
 		}
 		fetchUser();
 	}, [params.postId]);
 
-	
-	
+	function handleChange(event) {
+		setForm({ ...form, [event.target.name]: event.target.value });
+	}
 
 	async function handleDelete() {
 		await axios.delete(
@@ -35,21 +35,78 @@ function PostDetail() {
 		navigate("/forum");
 	}
 
+	async function handleSubmit(event) {
+		
+
+		const attPost = { ...user };
+		attPost.answers.push(form);
+
+		delete attPost._id;
+
+		await axios.put(
+			`https://ironrest.herokuapp.com/breakingbadforum/${params.postId}`,
+			attPost
+		);
+
+		navigate(`/forum`);
+	}
+	
 	return (
 		<div className="card-detail-page">
 			<div className="card-detail-container">
-				<h1 className="card-detail-name">
-					{user.name}{" "}
-				</h1>
+				<h1 className="card-detail-name">{user.name} </h1>
 				<span className="card-detail-email">{user.email} </span>
 				<div className="card-detail-post">
-					<p><strong>Post:</strong> {user.post}</p>
+					<p>
+						<strong>Post:</strong> {user.post}
+					</p>
 				</div>
 				<div>
 					<button className="delete-button" onClick={handleDelete}>
 						<strong>Delete!</strong>
 					</button>
 				</div>
+			</div>
+			
+			<div>
+			{user.answers.map((currentAnswer) => {
+				return (
+					<div> 
+					<p>{currentAnswer.answer}</p>
+					<span>by: {currentAnswer.owner}</span>
+					</div>
+				);
+			})}
+			</div>
+
+			<div>
+				<form onSubmit={handleSubmit}>
+					<div className="owner-div">
+						<input
+							type="text"
+							className="owner-input"
+							id="owner"
+							name="owner"
+							placeholder="Put your name here..."
+							value={form.owner}
+							onChange={handleChange}
+						/>
+						<label htmlFor="answer" className="">
+							<strong>Answer: </strong>
+						</label>
+						<input
+							type="answer"
+							className="answer-input"
+							id="answer"
+							name="answer"
+							value={form.answer}
+							onChange={handleChange}
+						/>
+					</div>
+					<button type="submit" className="answers-button">
+						Send!
+					</button>
+				</form>
 			</div>
 		</div>
 	);
